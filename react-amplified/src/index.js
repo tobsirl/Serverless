6f2883629eccca91a9cc9d705451f6c9f3b0d4a7
Paul Tobin <tobster111@gmail.com>
2020-09-10 14:59:49 +0100

    Added a backend GraphQL api with authenication

--- react-amplified/src/index.js
+++ react-amplified/src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import Amplify from "aws-amplify";
-import awsExports from "./aws-exports";
+import Amplify from 'aws-amplify';
+import awsExports from './aws-exports';
 Amplify.configure(awsExports);
 
 ReactDOM.render(
